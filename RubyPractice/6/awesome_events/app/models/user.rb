36e0e782fb40c3cd1df10fbbfc2cabfab398c5d4
Kazuya Matsumoto <kazuya.matsumoto@g-pc-5474.intra.gree-office.net>
2015-10-05 15:16:03 +0900

    event make form

--- RubyPractice/6/awesome_events/app/models/user.rb
+++ RubyPractice/6/awesome_events/app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+	has_many :created_events,class_name:'Event',foreign_key: :owner_id
 	def self.find_or_create_from_auth_hash(auth_hash)
 		provider = auth_hash[:provider]
 		uid = auth_hash[:uid]
@@ -10,5 +11,4 @@ class User < ActiveRecord::Base
 			user.image_url = image_url
 		end
 	end
-	
 end
