9dd7031b174c8b754b2b84035eaff4ba65c9c2ce
kazuooooo <matsumotokazuya7@gmail.com>
2015-10-05 22:36:37 +0900

    scaffold

---
+++ RubyPractice/railsbook_scaffold/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsbookScaffold::Application.config.secret_key_base = 'b1c5f36550d1c0750c3e9aeeb05ea57ba62567d40588edcc01117d8ffc2ab18d55618ea76496b4451ebdb94993ddb536130fde028b8ca686606fde89213af253'
---
+++ RubyPractice/railsbook_scaffold/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RailsbookScaffold::Application.config.session_store :cookie_store, key: '_railsbook_scaffold_session'
