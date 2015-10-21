d295a5273e4f167af8e432d06d25a2f7f26cb5e6
kazuooooo <matsumotokazuya7@gmail.com>
2015-10-21 22:42:48 +0900

    comment

--- RubyPractice/railsbook_scaffold/app/controllers/books_controller.rb
+++ RubyPractice/railsbook_scaffold/app/controllers/books_controller.rb
@@ -1,9 +1,13 @@
+#ApplicationControllerクラスを継承
 class BooksController < ApplicationController
+  #アクションが実行される前に:set_bookを実行する。onlyオプションで指定されたメソッドのときだけ実行される。
   before_action :set_book, only: [:show, :edit, :update, :destroy]
 
   # GET /books
   # GET /books.json
   def index
+    #@booksにBookオブジェクトを全て代入
+    #(@はインスタンス変数,@@はクラス変数)
     @books = Book.all
   end
 
@@ -14,6 +18,7 @@ class BooksController < ApplicationController
 
   # GET /books/new
   def new
+    #@bookにBookインスタンスを生成して代入
     @book = Book.new
   end
 
@@ -24,10 +29,15 @@ class BooksController < ApplicationController
   # POST /books
   # POST /books.json
   def create
+    #@bookにBookインスタンスをbook_paramsメソッドを用いて却ってきた値を用いて生成
     @book = Book.new(book_params)
 
+    #リクエストのformatに応じた処理を返す
     respond_to do |format|
+      #@model.saveで生成したオブジェクトをデータベースに保存する。結果のtrue/falseが返る
       if @book.save
+        #htmlリクエストのときは@モデルを指定することでそのモデルのshowのパスを指定したことになる??
+        #notice: redirect to メソッドのオプション通知メッセージを表示
         format.html { redirect_to @book, notice: 'Book was successfully created.' }
         format.json { render action: 'show', status: :created, location: @book }
       else
@@ -69,6 +79,8 @@ class BooksController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def book_params
+      #params(渡ってきた値)が:bookというキーを持っていれば、
+      #params[:book]は:isbn,:title,:price,...というキーを持つハッシュに対して更新を許可する都いう意味
       params.require(:book).permit(:isbn, :title, :price, :publish, :published, :cd)
     end
 end
